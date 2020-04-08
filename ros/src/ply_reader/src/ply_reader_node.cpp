65998d0820100a9d832e9aec2a38aacc175ffb11
Leon Stern <leon.stern@protonmail.com>
2020-04-08 14:01:50 +0200

    ros/src/ply_reader

---
+++ ros/src/ply_reader/src/ply_reader_node.cpp
@@ -0,0 +1,59 @@
+#include <ros/ros.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/io/ply_io.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <queue>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+typedef std::queue<std::string> StringQueue;
+typedef pcl::PointCloud<pcl::PointXYZ> PCloud;
+typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PCloudPtr;
+
+StringQueue getFileNamesInDictionary(const char* path);
+
+ros::Publisher ply_publisher;
+
+int main(int argc, char* argv[])
+{
+    ROS_INFO("Node started.");
+    ros::init(argc, argv, "ply_reader_node");
+    ros::NodeHandle node;
+    ply_publisher = node.advertise<sensor_msgs::PointCloud2>("/camera/depth/color/points", 1000);
+
+    StringQueue plys = getFileNamesInDictionary("/home/lstern/dhbw-screwing-robot/intelrealsense_d435/data/0_grad/recording_1/plys/");
+
+    while(ros::ok())
+    {
+        PCloudPtr pCloud(new PCloud());
+        if (pcl::io::loadPLYFile<pcl::PointXYZ>(plys.front(), *pCloud) == -1)
+        {
+            ROS_ERROR("Could not read *.ply-file.");
+            exit(0);
+        }
+        pCloud->header.frame_id = "/camera";
+        ply_publisher.publish(*pCloud);
+    }
+
+    ros::spin();
+    return 0;
+}
+
+StringQueue getFileNamesInDictionary(const char* path)
+{
+    StringQueue ply_paths;
+    DIR* dirp = opendir(path);
+    struct dirent * dp;
+    while((dp = readdir(dirp)) != NULL) 
+    {
+        std::string filename = dp->d_name;
+        ply_paths.push(path + filename);
+    }
+    closedir(dirp);
+    ply_paths.pop();
+    ply_paths.pop();
+    return ply_paths;
+}
