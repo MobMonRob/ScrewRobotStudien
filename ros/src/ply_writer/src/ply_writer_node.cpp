8e0ae7bf581964fde0ac0256a93138958fbc9adc
Leon Stern <leon.stern@protonmail.com>
2020-04-08 14:15:25 +0200

    ros/src/ply_writer

---
+++ ros/src/ply_writer/src/ply_writer_node.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/io/ply_io.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <queue>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PCloud;
+typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PCloudPtr;
+
+long counter = 0;
+
+void callback(const PCloud::ConstPtr& msg)
+{
+    pcl::io::savePLYFile<pcl::PointXYZ>("/home/lstern/ScrewRobotStudien/ros" + std::to_string(counter++) + ".ply", *msg);
+}
+
+int main(int argc, char* argv[])
+{
+    ROS_INFO("Node started.");
+    ros::init(argc, argv, "ply_writer_node");
+    ros::NodeHandle node;
+
+    ros::Subscriber sub = node.subscribe<PCloud>("/screw_pcloud", 1, callback);
+
+    ros::spin();
+    return 0;
+}
